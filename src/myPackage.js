0cba491106dc7b6e77f5b6350f1633dc6b358f94
ChanwooBrianKim <151323407+ChanwooBrianKim@users.noreply.github.com>
2024-07-23 23:38:58 +1000

    2nd commit - DefinitelyTyped

---
+++ src/myPackage.js
@@ -0,0 +1,21 @@
+// @ts-check
+/**
+ * Initialises the project
+ * @param {object} config 
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns {boolean}
+ */
+export function init(config) {
+    return true;
+}
+
+
+/**
+ * Exits the program
+ * @param {number} code 
+ * @returns 
+ */
+export function exit(code) {
+    return code + 1;
+}
